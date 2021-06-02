a42e98a4cc8041036347eae724865244ed6d99a6
Yash Lohiya <yashlohiya778@gmail.com>
2021-06-02 13:09:59 +0530

    First Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { Component } from 'react';
+import Menu from './Components/MenuComponent';
 
-function App() {
-      return (
-        <div className="App">
-          <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-          </Navbar>
-        </div>
-      );
-    }
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+
+  }
+}
 export default App;
